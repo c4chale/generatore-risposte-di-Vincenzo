38bb774766494705958ad1e5d8cc1f06703edd29
acwf547 <alessandro.candeloro@city.ac.uk>
2020-04-20 16:16:59 +0200

    adding html

--- random_string.js
+++ random_string.js
@@ -1,8 +1,26 @@
-var textArray = [
-    'Sentence 1',
-    'Sentence 2',
-    'Sentence 3 - fra gay',
-];
-var randomNumber = Math.floor(Math.random()*textArray.length);
+var $quoteBox = document.getElementById('quotebox');
 
-console.log(textArray[randomNumber])
+var quotesList = [
+  {
+    quote: "Planting popcorn does not produce more popcorn",
+    person: "Farmer Ted"
+  }, {
+    quote: "White whale, bad whale",
+    person: "Confucious (Moby Dick)"
+  }, {
+    quote: "Use the strobe function to disorientate your attacker",
+    person: "Flashlight"
+  }, {
+    quote: "Apply liberally to your erogenous zones",
+    person: "Spice Bomb"
+  }, {
+    quote: "Help me, I'm bleaching",
+    person: "The Great Barrier Reef"
+  }
+];
+
+function randomQuote() {
+  var randIdx = Math.floor(Math.random() * quotesList.length);
+  var randQuote = quotesList[randIdx];
+  $quoteBox.innerHTML = "\"" + randQuote.quote + "\" - " + randQuote.person;
+}
