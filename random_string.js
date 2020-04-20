84a6aceb9e90519979deda7b7462288fb4efe2d3
acwf547 <alessandro.candeloro@city.ac.uk>
2020-04-20 16:36:54 +0200

    refuso

--- random_string.js
+++ random_string.js
@@ -1,12 +1,3 @@
-// var textArray = [
-//     'Sentence 1',
-//     'Sentence 2',
-//     'Sentence 3 - fra gay',
-// ];
-// var randomNumber = Math.floor(Math.random()*textArray.length);
-
-// console.log(textArray[randomNumber])
-
 var $quoteBox = document.getElementById('quotebox');
 
 var quotesList = [
