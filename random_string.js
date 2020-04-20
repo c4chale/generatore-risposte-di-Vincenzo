1f6052f69757dab315ef679450dcfd63f48b95ec
chale96 <challenge.ale@gmail.com>
2020-04-20 15:09:50 +0200

    adding js

---
+++ random_string.js
@@ -0,0 +1,8 @@
+var textArray = [
+    'Sentence 1',
+    'Sentence 2',
+    'Sentence 3 - fra gay',
+];
+var randomNumber = Math.floor(Math.random()*textArray.length);
+
+console.log(textArray[randomNumber])
